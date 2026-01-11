2b7b182f9a53c2550455af17dda094797ddc2e16
Julio Cesar Sulato Filho <31974724+mazarotto-dev@users.noreply.github.com>
2026-01-11 19:53:24 -0300

    refactor(config): extract env schema to dedicated file

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,20 +1,6 @@
+import { envSchema } from '@/schemas/env.schema';
 import dotenv from 'dotenv';
-import { z } from 'zod';
 
 dotenv.config();
-
-const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-  PORT: z.coerce.number().default(3000),
-  SERVICE_NAME: z.string().default('weather-api'),
-  API_PREFIX: z.string().default('/api/v1'),
-
-  OPENWEATHER_API_KEY: z.string().min(1, 'OPENWEATHER_API_KEY is required'),
-  OPENWEATHER_BASE_URL: z
-    .url()
-    .default('https://api.openweathermap.org/data/2.5'),
-
-  DATABASE_URL: z.url('DATABASE_URL must be a valid URL'),
-});
 
 export const env = envSchema.parse(process.env);
---
+++ src/schemas/env.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  PORT: z.coerce.number().default(3000),
+  SERVICE_NAME: z.string().default('weather-api'),
+  API_PREFIX: z.string().default('/api/v1'),
+
+  OPENWEATHER_API_KEY: z.string().min(1, 'OPENWEATHER_API_KEY is required'),
+  OPENWEATHER_BASE_URL: z
+    .url()
+    .default('https://api.openweathermap.org/data/2.5'),
+
+  DATABASE_URL: z.url('DATABASE_URL must be a valid URL'),
+});
